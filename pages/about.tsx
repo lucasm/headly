f47f52245bfcd79bce06e295bb187400b45aea23
Lucas Menezes <lucasm@lucasm.dev>
2022-07-30 04:32:59 -0300

    feat: eslint, loader

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,19 +5,14 @@ import PageLayout from '../components/PageLayout'
 export default function PageAbout() {
   return (
     <PageLayout title="About - Headly" description="News reader app for the critical sense.">
-
- 
-        <section class="fullscreen">
-            <div class="fullscreen-container">
-
-                <div class="container">
-                    <h1>Headly</h1>
-                    <p>News for the critical sense</p>
-                </div>
-
-            </div>
-        </section>
-
+      <section className="fullscreen">
+        <div className="fullscreen-container">
+          <div className="container">
+            <h1>Headly</h1>
+            <p>News for the critical sense</p>
+          </div>
+        </div>
+      </section>
     </PageLayout>
   )
 }
